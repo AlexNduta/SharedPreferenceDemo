26d7bce9bdaec7af3219210c8b4146463d089267
Alex Kinyanjui Nduta <86476101+AlexNduta@users.noreply.github.com>
2022-08-21 18:05:43 +0300

    using shared preference by uisng save buttons

--- app/src/main/java/com/example/spdemo/MainActivity.kt
+++ app/src/main/java/com/example/spdemo/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.spdemo
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
-//declare global variables so as to be able to acces them from other functions in the main activity
+    //declare global variables so as to be able to acces them from other functions in the main activity
     private lateinit var nameText: EditText
     private lateinit var ageText: EditText
-//declare global variables that reference the sharedPreference object type
+private lateinit var save:Button
+private lateinit var Retrieve :Button
+    //declare global variables that reference the sharedPreference object type
     private lateinit var sf: SharedPreferences
     private lateinit var editor: SharedPreferences.Editor
     // the esitor is needed to edit values for shared preference
@@ -18,52 +21,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //access the golbal variables
-        nameText = findViewById(R.id.etName)
-        ageText = findViewById(R.id.etAge)
+        nameText= findViewById(R.id.etName)
+        ageText= findViewById(R.id.etAge)
+        save= findViewById(R.id.btnsave)
+        Retrieve= findViewById(R.id.btnRetrieve)
+// create the name of the folder and set the mode
+        val pref=getSharedPreferences("details", MODE_PRIVATE)
+val edit =pref.edit()
 
-// initializee the sharepreference
-        //"my sf" is the name of the file we will create to save the data
-        //MODE_PRIVATE -> access to our data is limited to our application
-        sf = getSharedPreferences("my sf", MODE_PRIVATE)
+        //get refrerence to our use input
+save.setOnClickListener {
+ val name = nameText.text.toString()
+    val age =ageText.text.toString().toInt()
 
-            //initialize the editor by calling the edit fuction
-        editor = sf.edit()
+    edit.apply {
+        putString("name", name)
+        putInt("age", age)
+
+        apply()
     }
-
-
-    // on pause of our application, the following code will  execute
-    override fun onPause() {
-        super.onPause()
-
-        //write code to get the user input values
-        val name = nameText.text.toString()
-        val age = ageText.text.toString().toInt()
-
-        //use editor to save the collected input into the shared preference using key-value pairs
-        editor.apply {
-//use key 'sf name" to store the value of the name variable
-            putString("sf_name", name)
-            //use the "sf_age"
-            putInt("sf_age", age)
-
-            //commitng saving the data
-            commit()
-        }
-    }
-
-// we use the onResume() to rectrieve the saved values
-    override fun onResume() {
-        super.onResume()
-    //get the name using the getString() and getInt() by passing the key used to store the value
-        val name = sf.getString("sf_name", null)
-        val age = sf.getInt("sf_sge", 0)
-    //display the editext
-        nameText.setText(name)
-//
-if (age != 0) 45
-    ageText.setText(age.toString())
 }
+Retrieve.setOnClickListener {
+    val name = pref.getString("name", null)
+    val age = pref.getInt("age", 0)
+
+    nameText.setText(name)
+    ageText.setText(age.toString())
+
+}
+
+
+}
+
 
     }
 
